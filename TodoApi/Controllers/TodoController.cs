0e4a03367e0df2c6f5fbc3280f86814be529f8d3
Ogunboyejo Olatunde <Ogunboyejo@tdafrica.com>
2018-11-27 14:03:57 +0100

    initial commit todo web api

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+            if(_context.TodoItems.Count() == 0 ) {
+                _context.TodoItems.Add(new TodoItem{Name="Code", IsComplete=true});
+                _context.SaveChanges();
+            }
+            
+            
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<TodoItem>> Get()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}", Name="GetTodo")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item != null) {
+                return item;
+            }
+            return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] TodoItem item)
+        {
+            if(item != null) {
+                _context.TodoItems.Add(item);
+                _context.SaveChanges();
+                return CreatedAtRoute("GetTodo", new { Id = item.Id},item);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] TodoItem item)
+        {
+            if (item != null) {
+                var todo = _context.TodoItems.Find(id);
+                if(todo != null) {
+                    todo.IsComplete = item.IsComplete;
+                    todo.Name = item.Name ?? todo.Name;
+                    _context.TodoItems.Update(todo);
+                    _context.SaveChanges();
+                    return NoContent();
+                }
+            }
+            return NotFound();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TodoApi/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var conn = "Server=.\\SQLEXPRESS;Database=todos2;integrated security=true";
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(conn));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseCors(builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+
+        }
+    }
+}
